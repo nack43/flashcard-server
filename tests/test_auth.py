2e1406bcba841d6628575a26f9f65af78fa0a0db
tekun43 <guringurin43@icloud.com>
2017-12-19 00:08:24 +0900

    Moved duplicated code into single function for auth test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -25,43 +25,42 @@ class AuthTestCase(unittest.TestCase):
             db.drop_all()
             db.create_all()
 
-
-    def test_login(self):
-        """Test registered user can login."""
-        res_1 = self.client().post(
+    def sign_up(self):
+        return self.client().post(
                 '/v1/users',
                 content_type='application/json',
                 data=json.dumps(self.user_data)
                 )
 
-        res_2 = self.client().post(
+
+    def test_login(self):
+        """Test registered user can login."""
+        self.sign_up()
+
+        res = self.client().post(
                 '/v1/authentication',
                 content_type='application/json',
                 data=json.dumps(self.user_data)
                 )
 
-        result = json.loads(res_2.data.decode())
+        result = json.loads(res.data.decode())
 
         self.assertEqual(result['message'], "You logged in successfully.")
-        self.assertEqual(login_res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
         self.assertTrue(['access_token'])
 
 
     def test_non_registered_user_login(self):
         """Test non registered users cannot login."""
-        res_1 = self.client().post(
-                '/v1/users',
-                content_type='application/json',
-                data=json.dumps(self.not_a_user)
-                )
+        self.sign_up()
         
-        res_2 = self.client().post(
+        res = self.client().post(
                 '/v1/authentication',
                 content_type='application/json',
                 data=json.dumps(self.not_a_user)
                 )
 
-        result = json.loads(res_2.data.decode())
+        result = json.loads(res.data.decode())
 
         self.assertEqual(res.status_code, 401)
         self.assertEqual(result['message'], "Invalid email or password.")
