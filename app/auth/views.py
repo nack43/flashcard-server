3620d7385b23d2004ada131c9e3a66a0b33175da
tekun43 <guringurin43@icloud.com>
2017-12-20 21:54:39 +0900

    Fixed the process which was pointed out

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,12 +16,12 @@ def login():
 
         user.generate_token(user.id)
 
-        if user.access_token:
-            response = {
-                'access_token': user.access_token.decode()
-            }
+        response = {
+             'access_token': user.access_token.decode()
+        }
 
-            return make_response(jsonify(response)), status.HTTP_200_OK
+        return make_response(jsonify(response)), status.HTTP_200_OK
+
     else:
 
         response = {
